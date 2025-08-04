544860e0b33612c97668ec2417331a1ebf230efa
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-04 11:41:37 +0200

    code task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,6 +9,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
@@ -16,18 +22,22 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (parent == NULL)
+	if (parent->left != NULL)
 	{
-		return (null)
+		binary_tree_t *old_left = parent->left;
+
+		new_node->left = old_left;
+		old_left->parent = new_node;
 	}
 	else
 	{
-		new_node->left = ;
-		new_node->right = NULL;
+		new_node->left = NULL;
 	}
 
+	new_node->right = NULL;
 	new_node->n = value;
 	new_node->parent = parent;
+	parent->left = new_node;
 
 	return (new_node);
 }
