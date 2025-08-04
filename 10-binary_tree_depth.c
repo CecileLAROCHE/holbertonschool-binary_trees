4a8c02a4ab649dd9b53abd2d11664fa3371ac182
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-04 17:22:40 +0200

    code tast 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,4 +7,20 @@
  * Return: Depth of the tree, or 0 if tree is NULL
  */
 
-size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->parent == NULL)
+	{
+		return (0);
+	}
+
+	binary_tree_depth(tree->parent);
+
+	return (1 + binary_tree_depth(tree->parent));
+}
