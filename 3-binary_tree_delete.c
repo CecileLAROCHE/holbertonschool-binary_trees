c546e276a0d67f8588f9ed4cc3417d7c50ec5c65
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-04 11:25:20 +0200

    creations des premiers fichiers et code tassk 0

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree);
