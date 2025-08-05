a69506eaf2971bfaef0e6a3a8e2a446afbc0096b
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-05 09:48:45 +0200

    code task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,4 +7,20 @@
  * Return: Balance factor of the tree, or 0 if tree is NULL
  */
 
-int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_left;
+	int height_right;
+
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	return (height_left - height_right);
+
+}
