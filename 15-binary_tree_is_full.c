1b0ec8d533824db9172720f698021a472ca1d546
CecileLAROCHE <11458@holbertonstudents.com>
2025-08-05 11:14:33 +0200

    code task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,4 +7,24 @@
  * Return: 1 if the tree is full, 0 otherwise
  */
 
-int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
